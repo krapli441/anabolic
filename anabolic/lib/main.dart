bee59d12f1e1e41d19c39ff273d080eedcb23d2c
박준형 <108733795+krapli441@users.noreply.github.com>
2023-09-25 14:29:35 +0900

    우선 마테리얼로 진행해보자.

--- anabolic/lib/main.dart
+++ anabolic/lib/main.dart
@@ -1,23 +1,23 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+// Flutter 프레임워크의 material 디자인 위젯을 사용하기 위해 필요한 패키지를 import 하였음.
 
+// 앱의 진입점 (Entry Point), 앱을 실행할 때 처음 호출되는 함수이다.
 void main() {
-  runApp(const CupertinoApp(
-    home: MyCupertinoApp(),
-  ));
+  runApp(const MyApp());
 }
 
-class MyCupertinoApp extends StatelessWidget {
-  const MyCupertinoApp({Key? key}) : super(key: key);
-
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return const CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(
-        middle: Text('ANABOLIC'), // 앱 바에 출력할 테스트
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: const Text('A'), // 앱 바에 출력할 테스트
       ),
-      child: Center(
+      body: const Center(
         child: Text('Hello, World!'), // 가운데에 출력할 테스트
       ),
-    );
+    ));
   }
 }
