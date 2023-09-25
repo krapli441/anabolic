a4f04268c1e74dc84f1cff08a123b3032e600dd5
박준형 <108733795+krapli441@users.noreply.github.com>
2023-09-25 14:23:07 +0900

    쿠퍼티노 디자인으로 변경해봄

--- anabolic/lib/main.dart
+++ anabolic/lib/main.dart
@@ -1,23 +1,23 @@
-import 'package:flutter/material.dart';
-// Flutter 프레임워크의 material 디자인 위젯을 사용하기 위해 필요한 패키지를 import 하였음.
+import 'package:flutter/cupertino.dart';
 
-// 앱의 진입점 (Entry Point), 앱을 실행할 때 처음 호출되는 함수이다.
 void main() {
-  runApp(const MyApp());
+  runApp(const CupertinoApp(
+    home: MyCupertinoApp(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyCupertinoApp extends StatelessWidget {
+  const MyCupertinoApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: const Text('Hello, World'), // 앱 바에 출력할 테스트
+    return const CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: Text('ANABOLIC'), // 앱 바에 출력할 테스트
       ),
-      body: const Center(
+      child: Center(
         child: Text('Hello, World!'), // 가운데에 출력할 테스트
       ),
-    ));
+    );
   }
 }
