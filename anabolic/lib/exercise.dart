f123e6520ac17c1a277bf87cd8ad44a50d8a31bc
박준형 <108733795+krapli441@users.noreply.github.com>
2023-10-04 15:36:44 +0900

    운동 추가하기 버튼 스타일링을 위해 dotted border 설치

--- anabolic/lib/exercise.dart
+++ anabolic/lib/exercise.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:dotted_border/dotted_border.dart';
 
 class ExerciseList extends StatefulWidget {
   final DateTime selectedDate;
@@ -18,21 +19,38 @@ class _ExerciseState extends State<ExerciseList> {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            const Text('운동 기록'),
             const SizedBox(height: 20), // 간격 조정
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => ExerciseRecord(),
+            DottedBorder(
+              borderType: BorderType.RRect,
+              radius: const Radius.circular(5),
+              padding: const EdgeInsets.all(6),
+              dashPattern: const [8, 4],
+              color: Colors.blue,
+              child: ClipRRect(
+                borderRadius: const BorderRadius.all(Radius.circular(5)),
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    elevation: MaterialStateProperty.all(0), // 그림자 제거
+                    minimumSize: MaterialStateProperty.all(const Size(350, 70)),
+                    backgroundColor: MaterialStateProperty.all(Colors.white),
                   ),
-                );
-              },
-              child: const Text('운동 추가하기'),
-            ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const ExerciseRecord(),
+                      ),
+                    );
+                  },
+                  child: const Text(
+                    '운동 추가하기',
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                ),
+              ),
+            )
           ],
         ),
       ),
