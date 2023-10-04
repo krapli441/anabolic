464bcd5154451250540c145e695a667f59412fc7
박준형 <108733795+krapli441@users.noreply.github.com>
2023-10-04 11:24:35 +0900

    상태 업데이트

--- anabolic/lib/diary.dart
+++ anabolic/lib/diary.dart
@@ -5,8 +5,15 @@ import 'package:table_calendar/table_calendar.dart';
 // Todo
 // 1. 달력 날짜를 클릭하면 해당 날짜에 이벤트를 추가할 수 있어야 함.
 
-class Diary extends StatelessWidget {
+class Diary extends StatefulWidget {
   const Diary({Key? key}) : super(key: key);
+
+  @override
+  _DiaryState createState() => _DiaryState();
+}
+
+class _DiaryState extends State<Diary> {
+  DateTime _selectedDate = DateTime.now();
 
   @override
   Widget build(BuildContext context) {
@@ -35,6 +42,11 @@ class Diary extends StatelessWidget {
                 lastDay: DateTime.utc(2030, 3, 14),
                 focusedDay: DateTime.now(),
                 locale: 'ko_KR',
+                onDaySelected: (selectedDay, focusedDay) {
+                  setState(() {
+                    _selectedDate = selectedDay;
+                  });
+                },
                 headerStyle: const HeaderStyle(
                   titleCentered: true,
                   formatButtonVisible: false,
@@ -89,9 +101,136 @@ class Diary extends StatelessWidget {
                   }
                   return Center(child: Text(date.day.toString()));
                 }),
-              )
+              ),
+              Text(
+                '${_selectedDate.year}년 ${_selectedDate.month}월 ${_selectedDate.day}일',
+                style: const TextStyle(fontSize: 20, color: Colors.blue),
+              ),
+              const SizedBox(height: 40),
+              const ExerciseStartButton()
             ],
           ),
+        ));
+  }
+}
+
+// class Diary extends StatelessWidget {
+//   const Diary({Key? key}) : super(key: key);
+
+//   @override
+// Widget build(BuildContext context) {
+//   return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('운동 기록'),
+//       ),
+//       body: Padding(
+//         padding: const EdgeInsets.all(8.0),
+//         child: Column(
+//           crossAxisAlignment: CrossAxisAlignment.center, // 텍스트를 중앙에 정렬
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             const Text(
+//               "운동을 시작해볼까요?\n기록할 날짜를 선택하세요.",
+//               style: TextStyle(
+//                 fontSize: 20,
+//                 fontWeight: FontWeight.normal,
+//                 color: Colors.blue,
+//               ),
+//               textAlign: TextAlign.center,
+//             ),
+//             const SizedBox(height: 20),
+//             TableCalendar(
+//               firstDay: DateTime.utc(2020, 10, 16),
+//               lastDay: DateTime.utc(2030, 3, 14),
+//               focusedDay: DateTime.now(),
+//               locale: 'ko_KR',
+//               headerStyle: const HeaderStyle(
+//                 titleCentered: true,
+//                 formatButtonVisible: false,
+//               ),
+//               daysOfWeekHeight: 20,
+//               calendarBuilders: CalendarBuilders(dowBuilder: (context, day) {
+//                 switch (day.weekday) {
+//                   case 1:
+//                     return const Center(
+//                       child: Text('월'),
+//                     );
+//                   case 2:
+//                     return const Center(
+//                       child: Text('화'),
+//                     );
+//                   case 3:
+//                     return const Center(
+//                       child: Text('수'),
+//                     );
+//                   case 4:
+//                     return const Center(
+//                       child: Text('목'),
+//                     );
+//                   case 5:
+//                     return const Center(
+//                       child: Text('금'),
+//                     );
+//                   case 6:
+//                     return const Center(
+//                       child: Text(
+//                         '토',
+//                         style: TextStyle(color: Colors.blue),
+//                       ),
+//                     );
+//                   case 7:
+//                     return const Center(
+//                       child: Text(
+//                         '일',
+//                         style: TextStyle(color: Colors.red),
+//                       ),
+//                     );
+//                   default:
+//                     return const Center(child: Text('Error'));
+//                 }
+//               }, defaultBuilder: (context, date, _) {
+//                 if (date.weekday == 7) {
+//                   return Center(
+//                       child: Text(
+//                     date.day.toString(),
+//                     style: const TextStyle(color: Colors.red),
+//                   ));
+//                 }
+//                 return Center(child: Text(date.day.toString()));
+//               }),
+//             ),
+//             const SizedBox(height: 40),
+//             const ExerciseStartButton()
+//           ],
+//         ),
+//       ));
+// }
+// }
+
+class ExerciseStartButton extends StatelessWidget {
+  const ExerciseStartButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => const Diary()));
+        },
+        style: ButtonStyle(
+          minimumSize: MaterialStateProperty.all(const Size(200, 50)),
+          backgroundColor: MaterialStateProperty.all(Colors.blue),
+        ),
+        child: const Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.play_arrow),
+            SizedBox(
+              width: 10,
+            ),
+            Text('시작하기'),
+          ],
         ));
   }
 }
