48db94ab4ccf570feb31756a8f35dc66d5a68f79
박준형 <108733795+krapli441@users.noreply.github.com>
2023-10-04 11:31:40 +0900

    클래스를 공개 클래스로 변경함

--- anabolic/lib/diary.dart
+++ anabolic/lib/diary.dart
@@ -9,10 +9,10 @@ class Diary extends StatefulWidget {
   const Diary({Key? key}) : super(key: key);
 
   @override
-  _DiaryState createState() => _DiaryState();
+  DiaryState createState() => DiaryState();
 }
 
-class _DiaryState extends State<Diary> {
+class DiaryState extends State<Diary> {
   DateTime _selectedDate = DateTime.now();
 
   @override
@@ -113,99 +113,6 @@ class _DiaryState extends State<Diary> {
         ));
   }
 }
-
-// class Diary extends StatelessWidget {
-//   const Diary({Key? key}) : super(key: key);
-
-//   @override
-// Widget build(BuildContext context) {
-//   return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('운동 기록'),
-//       ),
-//       body: Padding(
-//         padding: const EdgeInsets.all(8.0),
-//         child: Column(
-//           crossAxisAlignment: CrossAxisAlignment.center, // 텍스트를 중앙에 정렬
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: [
-//             const Text(
-//               "운동을 시작해볼까요?\n기록할 날짜를 선택하세요.",
-//               style: TextStyle(
-//                 fontSize: 20,
-//                 fontWeight: FontWeight.normal,
-//                 color: Colors.blue,
-//               ),
-//               textAlign: TextAlign.center,
-//             ),
-//             const SizedBox(height: 20),
-//             TableCalendar(
-//               firstDay: DateTime.utc(2020, 10, 16),
-//               lastDay: DateTime.utc(2030, 3, 14),
-//               focusedDay: DateTime.now(),
-//               locale: 'ko_KR',
-//               headerStyle: const HeaderStyle(
-//                 titleCentered: true,
-//                 formatButtonVisible: false,
-//               ),
-//               daysOfWeekHeight: 20,
-//               calendarBuilders: CalendarBuilders(dowBuilder: (context, day) {
-//                 switch (day.weekday) {
-//                   case 1:
-//                     return const Center(
-//                       child: Text('월'),
-//                     );
-//                   case 2:
-//                     return const Center(
-//                       child: Text('화'),
-//                     );
-//                   case 3:
-//                     return const Center(
-//                       child: Text('수'),
-//                     );
-//                   case 4:
-//                     return const Center(
-//                       child: Text('목'),
-//                     );
-//                   case 5:
-//                     return const Center(
-//                       child: Text('금'),
-//                     );
-//                   case 6:
-//                     return const Center(
-//                       child: Text(
-//                         '토',
-//                         style: TextStyle(color: Colors.blue),
-//                       ),
-//                     );
-//                   case 7:
-//                     return const Center(
-//                       child: Text(
-//                         '일',
-//                         style: TextStyle(color: Colors.red),
-//                       ),
-//                     );
-//                   default:
-//                     return const Center(child: Text('Error'));
-//                 }
-//               }, defaultBuilder: (context, date, _) {
-//                 if (date.weekday == 7) {
-//                   return Center(
-//                       child: Text(
-//                     date.day.toString(),
-//                     style: const TextStyle(color: Colors.red),
-//                   ));
-//                 }
-//                 return Center(child: Text(date.day.toString()));
-//               }),
-//             ),
-//             const SizedBox(height: 40),
-//             const ExerciseStartButton()
-//           ],
-//         ),
-//       ));
-// }
-// }
 
 class ExerciseStartButton extends StatelessWidget {
   const ExerciseStartButton({Key? key}) : super(key: key);
