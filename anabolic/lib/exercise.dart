506e9b9048fd553807108f1c373ebd045689b850
박준형 <108733795+krapli441@users.noreply.github.com>
2023-10-04 15:16:25 +0900

    운동 기록 페이지 작성 중

--- anabolic/lib/exercise.dart
+++ anabolic/lib/exercise.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 
 class ExerciseList extends StatefulWidget {
   final DateTime selectedDate;
-  const ExerciseList({Key? key, required this.selectedDate})
-      : super(key: key); // constructor에서 selectedDate를 받는다.
+  const ExerciseList({Key? key, required this.selectedDate}) : super(key: key);
 
   @override
   _ExerciseState createState() => _ExerciseState();
@@ -17,8 +16,41 @@ class _ExerciseState extends State<ExerciseList> {
         title: Text(
             '${widget.selectedDate.year}년 ${widget.selectedDate.month}월 ${widget.selectedDate.day}일'),
       ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('운동 기록'),
+            const SizedBox(height: 20), // 간격 조정
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ExerciseRecord(),
+                  ),
+                );
+              },
+              child: const Text('운동 추가하기'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ExerciseRecord extends StatelessWidget {
+  const ExerciseRecord({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('운동 기록'),
+      ),
       body: const Center(
-        child: Text('운동 기록'),
+        child: Text('여기서 운동을 기록할 수 있습니다.'),
       ),
     );
   }
